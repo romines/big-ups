d248a742dfd6d6e13d9fb9b703cb9b885c247720
Adam Romines <adam.romines@gmail.com>
2015-12-03 21:40:33 -0700

    very basic styling for directives

--- core/client/directives/matchup/matchup.directive.js
+++ core/client/directives/matchup/matchup.directive.js
@@ -3,7 +3,7 @@ var app = angular.module('app');
 app.directive('matchup', function () {
     return {
         restrict: 'E',
-
+        
         templateUrl: 'directives/matchup/matchup.template.html',
 
         scope: '='
