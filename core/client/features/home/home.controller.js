de0e3a3b632ab34476a595c23b05d23e3cf8a6ce
Adam Romines <adam.romines@gmail.com>
2015-12-14 13:16:55 -0700

    Adds basic filtering to client

--- core/client/features/home/home.controller.js
+++ core/client/features/home/home.controller.js
@@ -6,56 +6,18 @@ app.controller('homeController', ['$scope', '$filter', 'matchupService', homeCon
 
 function homeController($scope, $filter, matchupService) {
 
+  $scope.filter = '';
   getMatchups();
 
   function getMatchups() {
     matchupService.getMatchups()
-git       .then(function(data) {
+      .then(function(data) {
 
         $scope.matchUps = data;
       });
   }
 
-  // retrieves the min 'id' of a collection, used for the group ordering.
-  //you can use lodash instead. e.g: _.min(arr, 'id')
-  $scope.min = function(arr) {
-    return $filter('min')
-      ($filter('map')(arr, 'burScore'));
+  $scope.updateFilter = function(filter) {
+    $scope.userFilter = filter;
   }
-
-
-  // $scope.timeFilter = '';
-  //
-  // $scope.max = function(arr) {
-  //   return $filter('max')
-  //     ($filter('map')(arr, 'burScore'));
-  // }
-
-  $scope.getMatchupById = function(id) {
-
-    matchupService.getMatchupById(id)
-      .then(function(data) {
-
-        $scope.friend = data;
-      });
-  };
-
-
-  // $scope.postNewMatchup = function() {
-  //
-  //   var newMatchup = {
-  //     home: $scope.home,
-  //     away: $scope.away,
-  //     date: $scope.date,
-  //     imgUrl: 'https://cdn2.iconfinder.com/data/icons/social-aquiocons/128/Aquicon-Github.png'
-  //   };
-  //   matchupService.postNewMatchup(newMatchup)
-  //     .then(function() {
-  //
-  //       $scope.getNewMatchups();
-  //     });
-  //   $scope.home = "";
-  //   $scope.away = "";
-  //   $scope.date = "";
-  // };
 }
