957e0a7618b5943d30aa4ca771a2757b77fa51f7
Adam Romines <adam.romines@gmail.com>
2015-12-15 21:16:35 -0700

    Working on tag logic

--- core/client/directives/matchup/matchup.directive.js
+++ core/client/directives/matchup/matchup.directive.js
@@ -3,9 +3,18 @@ var app = angular.module('app');
 app.directive('matchup', function () {
     return {
         restrict: 'E',
-        
+
         templateUrl: 'directives/matchup/matchup.template.html',
 
-        scope: '='
+        scope: '@',
+
+        controller: function ($scope) {
+
+          
+
+          $scope.toggleRiv = false;
+          $scope.togglePlayoffs = false;
+          $scope.toggleFav = false;
+        }
     };
 });
